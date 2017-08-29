bec14a3c7d6697ce3eecb63fbddce470c4e59ba9
Austin Moore <am058613@ohio.edu>
2017-08-28 23:47:46 -0400

    Added an ability to tell if the internet messed up during parsing. Correctly implement the negative and positive range values for player.

--- urllibpractice.py
+++ urllibpractice.py
@@ -38,7 +38,7 @@ class team:
         self.player_count = 5
         self.players = [player() for i in range(self.player_count)]
         self.url = ' '
-        self.team_negative = 0.0 # The addition of all negative on ONE team
+        self.total_negative = 0.0 # The addition of all negative on ONE team
 
 class game:
     def __init__(self):
@@ -53,32 +53,43 @@ class game:
 url = input('Enter the HLTV URL: ')
 user_agent_spoof = {'User-Agent':'Mozilla/5.0'}
 req = urllib.request.Request(url, headers=user_agent_spoof)
-page = urllib.request.urlopen(req).read()
+try:
+    page = urllib.request.urlopen(req).read()
+except:
+    print('The internet fucked up')
+    quit()
 page = page.decode('utf-8')
 
 # Create the game
 the_game = game()
 
+print()
+print('Getting team and player stats. This may take a few moments.')
+print()
 # Find and save team names
 for i in range(the_game.team_count):
     the_game.teams[i].name = re.findall(r'<div class="team' + str(i + 1) + '-gradient">.*<img alt="(.*)" src', page)
     the_game.teams[i].name = the_game.teams[i].name[0]
-    print('Team ' + str(i + 1) + ' name: ' + the_game.teams[i].name)
+    #print('Team ' + str(i + 1) + ' name: ' + the_game.teams[i].name)
 
 # Find the url to all teams
 for i in range(the_game.team_count):
     the_game.teams[i].url = re.findall(r'<div class="team' + str(i + 1) + '-gradient"><a href="(/\w*/\d*/.*)"><img', page)
     the_game.teams[i].url = the_game.teams[i].url[0]
     the_game.teams[i].url = 'https://www.hltv.org' + the_game.teams[i].url
-    print('Team ' + str(i + 1) + ' URL: ' + the_game.teams[i].url)
+    #print('Team ' + str(i + 1) + ' URL: ' + the_game.teams[i].url)
 
-print()
+#print()
 
 # Team loop
 # Get player stats
 for i in range(the_game.team_count): # Open the URL to the teams one at a time
     req = urllib.request.Request(the_game.teams[i].url, headers=user_agent_spoof)
-    page = urllib.request.urlopen(req).read()
+    try:
+        page = urllib.request.urlopen(req).read()
+    except:
+        print('The internet fucked up')
+        quit()
     page = page.decode('utf-8')
 
     # Get player names and URLs
@@ -94,73 +105,83 @@ for i in range(the_game.team_count): # Open the URL to the teams one at a time
         # Assign names and main page URLs
         the_game.teams[i].players[i2].name = player_names[i2]
         the_game.teams[i].players[i2].url = player_urls[i2]
-        print('Player ' + str(i2 + 1) + ' name: ' + the_game.teams[i].players[i2].name)
-        print('Player ' + str(i2 + 1) + ' URL: ' + the_game.teams[i].players[i2].url)
+        #print('Player ' + str(i2 + 1) + ' name: ' + the_game.teams[i].players[i2].name)
+        #print('Player ' + str(i2 + 1) + ' URL: ' + the_game.teams[i].players[i2].url)
         
         # Open main player URLs and assign the URL to their detailed stats
         req = urllib.request.Request(the_game.teams[i].players[i2].url, headers=user_agent_spoof)
-        page = urllib.request.urlopen(req).read()
+        try:
+            page = urllib.request.urlopen(req).read()
+        except:
+            print('The internet fucked up')
+            quit()
         page = page.decode('utf-8')
         
         # Assign detailed stats URLs
         the_game.teams[i].players[i2].stats_url = re.findall(r'<a href="(/\w*/\w*/\d*/.*)" class', page)
         the_game.teams[i].players[i2].stats_url = the_game.teams[i].players[i2].stats_url[0]
         the_game.teams[i].players[i2].stats_url = 'https://www.hltv.org' + the_game.teams[i].players[i2].stats_url
-        print('Player ' + str(i2 + 1) + ' detailed URL: ' + the_game.teams[i].players[i2].stats_url)
+        #print('Player ' + str(i2 + 1) + ' detailed URL: ' + the_game.teams[i].players[i2].stats_url)
 
         # Open the detailed stats URLs
         req = urllib.request.Request(the_game.teams[i].players[i2].stats_url, headers=user_agent_spoof)
-        page = urllib.request.urlopen(req).read()
+        try:
+            page = urllib.request.urlopen(req).read()
+        except:
+            print('The internet fucked up')
+            quit()
         page = page.decode('utf-8')
 
         # Grab player stats
         # Grab kills
         the_game.teams[i].players[i2].kills = re.findall(r'<div class="stats-row"><span>total kills</span><span>(\d*)', page, re.I)
         the_game.teams[i].players[i2].kills = int(the_game.teams[i].players[i2].kills[0])
-        print('Player ' + str(i2 + 1) + ' kills: ' + str(the_game.teams[i].players[i2].kills))
+        #print('Player ' + str(i2 + 1) + ' kills: ' + str(the_game.teams[i].players[i2].kills))
 
         # Grab deaths
         the_game.teams[i].players[i2].deaths = re.findall(r'<div class="stats-row"><span>Total deaths</span><span>(\d*)', page, re.I)
         the_game.teams[i].players[i2].deaths = int(the_game.teams[i].players[i2].deaths[0])
-        print('Player ' + str(i2 + 1) + ' deaths: ' + str(the_game.teams[i].players[i2].deaths))
+        #print('Player ' + str(i2 + 1) + ' deaths: ' + str(the_game.teams[i].players[i2].deaths))
 
         # Grab rounds
         the_game.teams[i].players[i2].rounds = re.findall(r'<div class="stats-row"><span>Rounds played</span><span>(\d*)', page, re.I)
         the_game.teams[i].players[i2].rounds = int(the_game.teams[i].players[i2].rounds[0])
-        print('Player ' + str(i2 + 1) + ' rounds: ' + str(the_game.teams[i].players[i2].rounds))
+        #print('Player ' + str(i2 + 1) + ' rounds: ' + str(the_game.teams[i].players[i2].rounds))
 
         # Grab HLTV Rating
         the_game.teams[i].players[i2].hltv_rating = re.findall(r'"strong">(.*)</span></div>', page, re.I)
         the_game.teams[i].players[i2].hltv_rating = float(the_game.teams[i].players[i2].hltv_rating[0])
-        print('Player ' + str(i2 + 1) + ' HLTV Rating: ' + str(the_game.teams[i].players[i2].hltv_rating))
+        #print('Player ' + str(i2 + 1) + ' HLTV Rating: ' + str(the_game.teams[i].players[i2].hltv_rating))
 
         # Calculate kdr
         the_game.teams[i].players[i2].kdr = the_game.teams[i].players[i2].kills / the_game.teams[i].players[i2].deaths
-        print('Player ' + str(i2 + 1) + ' KDR: ' + str(the_game.teams[i].players[i2].kdr))
+        #print('Player ' + str(i2 + 1) + ' KDR: ' + str(the_game.teams[i].players[i2].kdr))
 
         # Calculate dkr
         the_game.teams[i].players[i2].dkr = the_game.teams[i].players[i2].deaths / the_game.teams[i].players[i2].kills
-        print('Player ' + str(i2 + 1) + ' DKR: ' + str(the_game.teams[i].players[i2].dkr))
+        #print('Player ' + str(i2 + 1) + ' DKR: ' + str(the_game.teams[i].players[i2].dkr))
 
         # Calculate kpr
         the_game.teams[i].players[i2].kpr = the_game.teams[i].players[i2].kills / the_game.teams[i].players[i2].rounds
-        print('Player ' + str(i2 + 1) + ' KPR: ' + str(the_game.teams[i].players[i2].kpr))
+        #print('Player ' + str(i2 + 1) + ' KPR: ' + str(the_game.teams[i].players[i2].kpr))
 
         # Calculate dpr
         the_game.teams[i].players[i2].dpr = the_game.teams[i].players[i2].deaths / the_game.teams[i].players[i2].rounds
-        print('Player ' + str(i2 + 1) + ' DPR: ' + str(the_game.teams[i].players[i2].dpr))
+        #print('Player ' + str(i2 + 1) + ' DPR: ' + str(the_game.teams[i].players[i2].dpr))
 
         # Calculate inv_hltv_rating
         the_game.teams[i].players[i2].inv_hltv_rating = pow(the_game.teams[i].players[i2].hltv_rating, (-1))
-        print('Player ' + str(i2 + 1) + ' Inverse HLTV Rating: ' + str(the_game.teams[i].players[i2].inv_hltv_rating))
+        #print('Player ' + str(i2 + 1) + ' Inverse HLTV Rating: ' + str(the_game.teams[i].players[i2].inv_hltv_rating))
 
         
-        print()
+        #print()
 
 # Calculate the likelihood that a player is to get a kill or die
 def calculate_player_range():
     negative_upper = 0.0 # The current number to set the next player's lower_negative_range
     positive_upper = 0.0 # The current number to set the next player's lower_positive_range
+    for i in range(the_game.team_count):
+        the_game.teams[i].total_negative = 0.0 # The addition of all negative on ONE team
     total_positive = 0.0 # The addition of all positive on each team
 
     # Find total_positive and total_negative (for the teams)
@@ -170,36 +191,39 @@ def calculate_player_range():
                 total_positive += the_game.teams[i].players[i2].positive
 
     # Find the lower and upper ranges for both negative and positive
+    print ('Total positive: ' + str(total_positive))
     for i in range(the_game.team_count):
-            for i2 in range(the_game.teams[i].player_count):
-                the_game.teams[i].players[i2].lower_negative_range = negative_upper
-                the_game.teams[i].players[i2].upper_negative_range = the_game.teams[i].players[i2].negative / the_game.teams[i].total_negative
-                negative_upper = numpy.nextafter(the_game.teams[i].players[i2].upper_negative_range, 1)
-                the_game.teams[i].players[i2].lower_positive_range = positive_upper
-                the_game.teams[i].players[i2].upper_positive_range = the_game.teams[i].players[i2].positive / total_positive
-                positive_upper = numpy.nextafter(the_game.teams[i].players[i2].upper_positive_range, 1)
+        print('Team ' + str(i + 1) + ' Total Negative: ' + str(the_game.teams[i].total_negative))
+        negative_upper = 0.0
+        for i2 in range(the_game.teams[i].player_count):
+            the_game.teams[i].players[i2].lower_negative_range = negative_upper
+            the_game.teams[i].players[i2].upper_negative_range = the_game.teams[i].players[i2].lower_negative_range + (the_game.teams[i].players[i2].negative / the_game.teams[i].total_negative)
+            negative_upper = numpy.nextafter(the_game.teams[i].players[i2].upper_negative_range, 1)
+            the_game.teams[i].players[i2].lower_positive_range = positive_upper
+            the_game.teams[i].players[i2].upper_positive_range = the_game.teams[i].players[i2].lower_positive_range + (the_game.teams[i].players[i2].positive / total_positive)
+            positive_upper = numpy.nextafter(the_game.teams[i].players[i2].upper_positive_range, 1)
 
 def print_all_player_stats():
     for i in range(the_game.team_count):
             for i2 in range(the_game.teams[i].player_count):
-                print('Team ' + str(i) + ' Player ' + str(i2) + ' Name: ' + the_game.teams[i].players[i2].name)
-                print('Team ' + str(i) + ' Player ' + str(i2) + ' Kills: ' + str(the_game.teams[i].players[i2].kills))
-                print('Team ' + str(i) + ' Player ' + str(i2) + ' Deaths: ' + str(the_game.teams[i].players[i2].deaths))
-                print('Team ' + str(i) + ' Player ' + str(i2) + ' KDR: ' + str(the_game.teams[i].players[i2].kdr))
-                print('Team ' + str(i) + ' Player ' + str(i2) + ' DKR: ' + str(the_game.teams[i].players[i2].dkr))
-                print('Team ' + str(i) + ' Player ' + str(i2) + ' KPR: ' + str(the_game.teams[i].players[i2].kpr))
-                print('Team ' + str(i) + ' Player ' + str(i2) + ' DPR: ' + str(the_game.teams[i].players[i2].dpr))
-                print('Team ' + str(i) + ' Player ' + str(i2) + ' HLTV Rating: ' + str(the_game.teams[i].players[i2].hltv_rating))
-                print('Team ' + str(i) + ' Player ' + str(i2) + ' Inverse HLTV Rating: ' + str(the_game.teams[i].players[i2].inv_hltv_rating))
-                print('Team ' + str(i) + ' Player ' + str(i2) + ' URL: ' + the_game.teams[i].players[i2].url)
-                print('Team ' + str(i) + ' Player ' + str(i2) + ' Detailed URL: ' + the_game.teams[i].players[i2].stats_url)
-                print('Team ' + str(i) + ' Player ' + str(i2) + ' Negative: ' + str(the_game.teams[i].players[i2].negative))
-                print('Team ' + str(i) + ' Player ' + str(i2) + ' Positive: ' + str(the_game.teams[i].players[i2].positive))
-                print('Team ' + str(i) + ' Player ' + str(i2) + ' Lower Negative Range: ' + str(the_game.teams[i].players[i2].lower_negative_range))
-                print('Team ' + str(i) + ' Player ' + str(i2) + ' Upper Negative Range: ' + str(the_game.teams[i].players[i2].upper_negative_range))
-                print('Team ' + str(i) + ' Player ' + str(i2) + ' Lower Positive Range: ' + str(the_game.teams[i].players[i2].lower_positive_range))
-                print('Team ' + str(i) + ' Player ' + str(i2) + ' Upper Positive Range: ' + str(the_game.teams[i].players[i2].upper_positive_range))
-                print('Team ' + str(i) + ' Player ' + str(i2) + ' Alive: ' + str(the_game.teams[i].players[i2].alive))
+                print('Team ' + str(i + 1) + ' Player ' + str(i2 + 1) + ' Name: ' + the_game.teams[i].players[i2].name)
+                print('Team ' + str(i + 1) + ' Player ' + str(i2 + 1) + ' Kills: ' + str(the_game.teams[i].players[i2].kills))
+                print('Team ' + str(i + 1) + ' Player ' + str(i2 + 1) + ' Deaths: ' + str(the_game.teams[i].players[i2].deaths))
+                print('Team ' + str(i + 1) + ' Player ' + str(i2 + 1) + ' KDR: ' + str(the_game.teams[i].players[i2].kdr))
+                print('Team ' + str(i + 1) + ' Player ' + str(i2 + 1) + ' DKR: ' + str(the_game.teams[i].players[i2].dkr))
+                print('Team ' + str(i + 1) + ' Player ' + str(i2 + 1) + ' KPR: ' + str(the_game.teams[i].players[i2].kpr))
+                print('Team ' + str(i + 1) + ' Player ' + str(i2 + 1) + ' DPR: ' + str(the_game.teams[i].players[i2].dpr))
+                print('Team ' + str(i + 1) + ' Player ' + str(i2 + 1) + ' HLTV Rating: ' + str(the_game.teams[i].players[i2].hltv_rating))
+                print('Team ' + str(i + 1) + ' Player ' + str(i2 + 1) + ' Inverse HLTV Rating: ' + str(the_game.teams[i].players[i2].inv_hltv_rating))
+                print('Team ' + str(i + 1) + ' Player ' + str(i2 + 1) + ' URL: ' + the_game.teams[i].players[i2].url)
+                print('Team ' + str(i + 1) + ' Player ' + str(i2 + 1) + ' Detailed URL: ' + the_game.teams[i].players[i2].stats_url)
+                print('Team ' + str(i + 1) + ' Player ' + str(i2 + 1) + ' Negative: ' + str(the_game.teams[i].players[i2].negative))
+                print('Team ' + str(i + 1) + ' Player ' + str(i2 + 1) + ' Positive: ' + str(the_game.teams[i].players[i2].positive))
+                print('Team ' + str(i + 1) + ' Player ' + str(i2 + 1) + ' Lower Negative Range: ' + str(the_game.teams[i].players[i2].lower_negative_range))
+                print('Team ' + str(i + 1) + ' Player ' + str(i2 + 1) + ' Upper Negative Range: ' + str(the_game.teams[i].players[i2].upper_negative_range))
+                print('Team ' + str(i + 1) + ' Player ' + str(i2 + 1) + ' Lower Positive Range: ' + str(the_game.teams[i].players[i2].lower_positive_range))
+                print('Team ' + str(i + 1) + ' Player ' + str(i2 + 1) + ' Upper Positive Range: ' + str(the_game.teams[i].players[i2].upper_positive_range))
+                print('Team ' + str(i + 1) + ' Player ' + str(i2 + 1) + ' Alive: ' + str(the_game.teams[i].players[i2].alive))
                 print()
                 
 
@@ -223,6 +247,7 @@ for current_simulation in range(simulations):
                 the_game.teams[i].players[i2].positive = the_game.teams[i].players[i2].hltv_rating
 
     print()
+    calculate_player_range()
     print_all_player_stats()
     '''            
     trials = 10000
